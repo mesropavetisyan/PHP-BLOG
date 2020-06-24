61a8fa9b47314d2ebb508179591f6f98640a9dc1
Mesrop <mesropavetisyan94@yandex.ru>
2020-06-24 15:12:03 +0500

    register to login page

--- app/controllers/register-controller.php
+++ app/controllers/register-controller.php
@@ -22,12 +22,15 @@ function validateRegister()
     if(empty($_POST['name'])){
         $data['name']['error-message'] = "Name is required";
     }else {
-        $data['name']['value'] = $_POST['name'];
+        $data['name']['value'] = strip_tags($_POST['name']);
     }
     if(empty($_POST['email'])){
         $data['email']['error-message'] = "Email is required";
     }else {
-        $data['email']['value'] = $_POST['email'];
+        $data['email']['value'] = strip_tags($_POST['email']);
+        if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $data['email']['error-message'] = "Email is not correct";
+        }
     }
     if(empty($_POST['password'])){
         $data['password']['error-message'] = "Password is required";
@@ -37,4 +40,20 @@ function validateRegister()
     return $data;
 }
 
+function IssetErrors ($data)
+{
+    if(empty($_POST)) {
+        return true;
+    }
+    foreach ($data as $info) {
+        if (strlen($info['error-message']) > 0) {
+            return true;
+        }
+    }
+    return false;
+}
 $data = validateRegister();
+
+if (!IssetErrors($data)) {
+    header("Location:?p=login");
+}
