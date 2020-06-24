d7ba91654e638e949148cfa7d0e4e46f3ca701f2
Mesrop <mesropavetisyan94@yandex.ru>
2020-06-24 16:38:37 +0500

    login to profile page

--- app/templates/login.php
+++ app/templates/login.php
@@ -7,14 +7,18 @@
                     <form class="form-horizontal contact-form" role="form" method="post" action="">
                         <div class="form-group">
                             <div class="col-md-12">
-                                <input type="text" class="form-control" id="email" name="email"
-                                       placeholder="Email">
+                                <input type="email" class="form-control" id="email" name="email"
+                                       placeholder="Email"
+                                       value="<?= (!empty($data['email']['value'])) ?  $data['email']['value'] : "" ?>">
+                                <?= (!empty($data['email']['error-message'])) ? "<p class='warning-message'>" . $data['email']['error-message'] . " </p>" : "" ?>
                             </div>
                         </div>
                         <div class="form-group">
                             <div class="col-md-12">
-                                <input type="text" class="form-control" id="password" name="password"
-                                       placeholder="password">
+                                <input type="password" class="form-control" id="password" name="password"
+                                       placeholder="password"
+                                       value="<?= (!empty($data['password']['value'])) ?  $data['password']['value'] : "" ?>">
+                                <?= (!empty($data['password']['error-message'])) ? "<p class='warning-message'>" . $data['password']['error-message'] . " </p>" : "" ?>
                             </div>
                         </div>
                         <button type="submit" name="submit" class="btn send-btn">Login</button>
