f4e9c76ba89f6b8db9b77f31970b67b253308421
Mesrop <mesropavetisyan94@yandex.ru>
2020-06-30 23:25:28 +0500

    log out

---
+++ app/templates/logout.php
@@ -0,0 +1,13 @@
+<div class="col-md-8">
+
+    <div class="leave-comment mr0"><!--leave comment-->
+
+        <h3 class="text-uppercase">Do you want to log out</h3>
+        <br>
+        <form class="form-horizontal contact-form" role="form" method="post" action="">
+            <button type="submit" name="submit" class="btn send-btn" value="yes">Yes</button>
+            <a href="?p=home" class="btn">Cancel</a>
+
+        </form>
+    </div><!--end leave comment-->
+</div>
