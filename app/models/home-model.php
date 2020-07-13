470067389027deb46fbfa3fe665f373fcde52853
Mesrop <mesropavetisyan94@yandex.ru>
2020-07-13 14:44:57 +0500

    categories group concat

--- app/models/home-model.php
+++ app/models/home-model.php
@@ -2,12 +2,14 @@
 
 function getAllBlogPosts(){
     global $conn;
-    $sql = "SELECT blog_posts.id,title,url,author,date,image,LEFT(`text`,400) as 'text' , categories.name as 'category' FROM blog_posts 
+    $sql = "SELECT blog_posts.id,title,url,author,date,image,LEFT(`text`,400) as 'text' ,
+GROUP_CONCAT(categories.id, '=', categories.name SEPARATOR ',') as 'category' FROM blog_posts 
 JOIN blog_post_categories 
 ON blog_posts.id = blog_post_id 
 JOIN categories
 ON category_id = categories.id
-WHERE isActive =1";
+WHERE isActive =1
+GROUP BY blog_posts.id";
     $res = mysqli_query($conn,$sql);
     return $res;
 }
